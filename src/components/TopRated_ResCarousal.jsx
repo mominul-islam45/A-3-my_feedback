416aa0069a4fb8cb3092a7f4f9fb75daa1bf6c5c
mominul-islam45 <mominul4586@gmail.com>
2025-02-15 21:40:28 +0600

    top rated restaurant carousal added to home page

---
+++ src/components/TopRated_ResCarousal.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react'
+import Title from './common/Title'
+import { Card, CardBody, CardFooter, } from '@heroui/react'
+import { GoArrowLeft, GoArrowRight } from "react-icons/go";
+
+const TopRated_ResCarousal = () => {
+    // Fetching restaurant data from API
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        fetch('./db/restaurantsData.json')
+            .then((response) => response.json())
+            .then((jsonData) => {
+                setData(jsonData)
+            })
+    }, [])
+
+    // Filtering data for top rated(5 star) restaurants
+    const topRated = data.filter(item => item.rating === 5)
+
+    // on every click there will be new slide of 4 items
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [itemsPerSlide, setItemsPerSlide] = useState(4);
+    const slides = [];
+  
+    // For responsiveness
+    useEffect(() => {
+      const updateItemsPerSlide = () => {
+        if (window.innerWidth < 640) {
+          setItemsPerSlide(1);
+        } else if (window.innerWidth < 1024) {
+          setItemsPerSlide(2);
+        } else {
+          setItemsPerSlide(4);
+        }
+      };
+      
+      updateItemsPerSlide();
+      window.addEventListener("resize", updateItemsPerSlide);
+      return () => window.removeEventListener("resize", updateItemsPerSlide);
+    }, []);
+
+    // on every click there will be new slide of 4 items
+    for (let i = 0; i < topRated.length; i += itemsPerSlide){
+        slides.push(topRated.slice(i, i + itemsPerSlide))
+    }
+
+    const prevBtn = ()=>{
+        setCurrentIndex(currentIndex === 0? 0 : currentIndex - 1)
+    }
+    const nextBtn = ()=>{
+        setCurrentIndex(currentIndex === slides.length -1 ? slides.length-1 : currentIndex + 1)
+    }
+    // Indicator
+    const goToSlide = (i)=>{
+        setCurrentIndex(i)
+    }
+
+  return (
+    <div className='bg-blue py-4 px-10 rounded-xl w-full relative'>
+        <Title className='text-white font-normal'>Find the best restaurant ratings below</Title>
+        {/* Slide Container */}
+        <div className='grid lg:grid-cols-4 sm:grid-cols-2 gap-6 py-8 touch-pan-x'>
+            {slides.length > 0 && slides[currentIndex].map(({id, name, description, rating, total_ratings, img})=>(
+                <Card key={id} className='bg-light rounded-xl basis-full'>
+                        <img src={img[0]} alt="Image" className='rounded-xl object-cover h-[200px] aspect-[1.2/1]' />
+                    <CardBody className='space-y-1'>
+                        <h4 className='text-lg font-semibold cursor-pointer hover:underline underline-offset-3 decoration-2'>{name}</h4>
+                        <p className='text-sm text-gray'>{description}</p>
+                    </CardBody>
+                    <CardFooter className='text-sm space-x-1.5'>
+                        <img src="icons/5_star.svg" alt="Icon" />
+                        <span className='font-semibold ml-1.5'>{rating}</span>
+                        <span className='text-gray'>({total_ratings})</span>
+                    </CardFooter>
+                </Card>
+            ))}
+        </div>
+
+        {/* Slide Controllers */}
+        <button onClick={prevBtn} className={`bg-white rounded-full size-12 flex justify-center items-center absolute bottom-1/2 left-[16px] cursor-pointer hover:opacity-75 ${currentIndex === 0 && 'opacity-75'}`}><GoArrowLeft className='text-2xl stroke-1'/> </button>
+        <button onClick={nextBtn} className={`bg-white rounded-full size-12 flex justify-center items-center absolute bottom-1/2 right-[16px] cursor-pointer hover:opacity-75 ${currentIndex === slides.length -1 && 'opacity-75'}`}><GoArrowRight className='text-2xl stroke-1'/> </button>
+        
+        {/* Slider Indicator */}
+        <div className='flex justify-center gap-x-3 '>
+        {slides.map((slide,i)=>(
+          <div key={i}>
+            <div onClick={()=> goToSlide(i)} className={`size-[10px] rounded-full ${i === currentIndex ? 'bg-white': 'bg-gray_v2'} cursor-pointer`}></div>
+          </div>
+        ))}
+        </div>
+    </div>
+  )
+}
+
+export default TopRated_ResCarousal
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavbarArea from "../components/NavbarArea";
 import Footer from "../components/Footer";
 import Container_1 from "../components/common/Container_1";
+import TopRated_ResCarousal from "../components/TopRated_ResCarousal";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
         <div className="hidden md:block py-5">
           <img src="imgs/tab_bar.png" alt="Tab bar" />
         </div>
+        <TopRated_ResCarousal/>
       </Container_1>
       <Footer />
     </div>
